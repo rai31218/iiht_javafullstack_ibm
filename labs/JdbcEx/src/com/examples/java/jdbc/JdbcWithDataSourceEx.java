3a7e9a5fef3148e8e576d8f7e00590d589c55fad
saravana <saravana>
2019-03-15 15:46:23 +0530

    add jdbc examples

---
+++ labs/JdbcEx/src/com/examples/java/jdbc/JdbcWithDataSourceEx.java
@@ -0,0 +1,90 @@
+package com.examples.java.jdbc;
+
+//STEP 1. Import required packages
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
+public class JdbcWithDataSourceEx {
+
+	public static void main(String[] args) {
+
+		Connection conn = null;
+		Statement stmt = null;
+		try {
+			// STEP 2: Create Datasource instance
+			MysqlDataSource dataSource = new MysqlDataSource();
+			dataSource.setDatabaseName("jdbctraining");
+			dataSource.setUser("training");
+			dataSource.setPassword("training");
+			dataSource.setServerName("localhost");			
+
+			// STEP 3: Open a connection
+			System.out.println("Connecting to database...");			
+			conn = dataSource.getConnection();
+
+			// STEP 4: Execute a query
+			System.out.println("Creating statement...");
+			stmt = conn.createStatement();
+
+			String sql1, sql2;
+			sql1 = "SELECT id, name, age, designation FROM employee";
+//			 sql1 = "SELECT id, name, age FROM employee WHERE age >= 35 AND department='Admin' ORDER BY name";
+			sql2 = "UPDATE employee SET designation=? WHERE id=?";
+
+			PreparedStatement pstmt = conn.prepareStatement(sql2);
+			pstmt.setString(1, "Manager");
+			pstmt.setInt(2, 1);
+
+//			boolean status = stmt.execute(sql2);
+			boolean status = pstmt.execute();
+			System.out.println("Update status: " + status);
+			ResultSet rs = stmt.executeQuery(sql1);
+
+			// STEP 5: Extract data from result set
+			while (rs.next()) {
+				// Retrieve by column name
+				int id = rs.getInt("id");
+				int age = rs.getInt("age");
+				String name = rs.getString("name");
+				String designation = rs.getString("designation");
+
+				// Display values
+				System.out.print("ID: " + id);
+				System.out.print(", Age: " + age);
+				System.out.print(", Name: " + name);
+				System.out.print(", Designation: " + designation);
+
+				System.out.println();
+			}
+			// STEP 6: Clean-up environment
+			rs.close();
+			stmt.close();
+			conn.close();
+		} catch (SQLException se) {
+			// Handle errors for JDBC
+			se.printStackTrace();
+		} catch (Exception e) {
+			// Handle errors for Class.forName
+			e.printStackTrace();
+		} finally {
+			// finally block used to close resources
+			try {
+				if (stmt != null)
+					stmt.close();
+			} catch (SQLException se2) {
+			} // nothing we can do
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			} // end finally try
+		} // end try
+		System.out.println("Goodbye!");
+	}// end main
+}// end FirstExample
